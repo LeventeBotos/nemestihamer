7c3843078c21bde3b72d889a190b9cad67d6147e
Levente Botos <botos.levente2007@gmail.com>
2023-11-15 12:15:12 +0100

    done with the competition

---
+++ 2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static int PerformOperation(int number)
+        {
+            return number; // You can perform any operations on the integer here
+        }
+
+        // Function for math operator input
+        public static int initVerem(int inputSzam = 0, char muveletiJel = ' ')
+        {
+            var stack = new Stack<int>();
+            if (muveletiJel == ' ')
+            {
+                stack.Push(inputSzam);
+                return 1;
+            }
+            else
+            {
+                int operand2 = stack.Pop();
+                int operand1 = stack.Pop();
+                int result;
+
+                switch (muveletiJel)
+                {
+                    case '+':
+                        result = operand1 + operand2;
+                        break;
+                    case '-':
+                        result = operand1 - operand2;
+                        break;
+                    case '*':
+                        result = operand1 * operand2;
+                        break;
+                    case '/':
+                        if (operand2 != 0)
+                            result = operand1 / operand2;
+                        else
+                            throw new DivideByZeroException("Nem lehet osztani 0-val");
+                        break;
+                    default:
+                        throw new ArgumentException("Nincs ilyen műveleti jel");
+                }
+
+                stack.Push(result);
+                Console.WriteLine(result);
+                return result;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(initVerem(1));
+            Console.WriteLine(initVerem(2));
+            Console.WriteLine(initVerem('/'));
+
+        }
+    }
+}
---
+++ 7/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+class SikloErnyozes
+{
+    static void Main()
+    {
+        // Hegycsúcsok magasságai
+        int[] magassagok = { 7, 8, 7, 9, 3, 2, 4, 3, 3, 6 };
+
+        int n = 0;
+        // A, B, és C részekre vonatkozó számítások
+        int aValasz = SzamolSikloErnyozesek(magassagok, 1, n);
+        int bValasz = SzamolSikloErnyozesek(magassagok, 2, n);
+        int dValasz = SzamolSikloErnyozesek(magassagok, 3, n);
+        int eValasz = SzamolSikloErnyozesek(magassagok, 4, n);
+        int fValasz = SzamolSikloErnyozesek(magassagok, 5, n);
+        int gValasz = SzamolSikloErnyozesek(magassagok, 6, n);
+        int cValasz = aValasz + bValasz + eValasz + dValasz + fValasz + gValasz;
+
+        // Eredmények kiírása
+        Console.WriteLine($"A. {aValasz} különböző siklóernyzés");
+        Console.WriteLine($"B. {bValasz} különböző siklóernyzés");
+        Console.WriteLine($"C. {cValasz} különböző siklóernyzés összesen");
+    }
+
+    static int SzamolSikloErnyozesek(int[] magassagok, int atrepulesSzam, int hanyadik)
+    {
+        int eredmeny = 0;
+        int hossz = magassagok.Length;
+
+        for (int i = 0; i < hossz; i++)
+        {
+            if (magassagok[hanyadik] > magassagok[i])
+                eredmeny += SzamolErnyozesekRekurzivan(magassagok, i, atrepulesSzam - 1, hanyadik);
+        }
+
+        return eredmeny;
+    }
+
+    static int SzamolErnyozesekRekurzivan(int[] magassagok, int jelenlegiIndex, int atrepulesSzam, int hanyadik)
+    {
+        if (atrepulesSzam == 0)
+        {
+            return 1; // Ha nincs több átrepülés, akkor egy újabb siklóernyőzés kész
+        }
+
+        int eredmeny = 0;
+
+        for (int kovetkezoIndex = 0; kovetkezoIndex < magassagok.Length; kovetkezoIndex++)
+        {
+            if (magassagok[kovetkezoIndex] < magassagok[jelenlegiIndex])
+            {
+                eredmeny += SzamolErnyozesekRekurzivan(magassagok, kovetkezoIndex, atrepulesSzam - 1, hanyadik);
+            }
+        }
+
+        return eredmeny;
+    }
+
+    static int SzamolOsszesSikloErnyozes(int[] magassagok, int hanyadik)
+    {
+        int osszeg = 0;
+        int hossz = magassagok.Length;
+
+        for (int i = 1; i <= hossz; i++)
+        {
+            if (magassagok[i - 1] > magassagok[hanyadik])
+            {
+                osszeg += SzamolSikloErnyozesek(magassagok, i, hanyadik);
+            }
+        }
+
+        return osszeg;
+    }
+}
